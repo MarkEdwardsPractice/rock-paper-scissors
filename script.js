49cdbbcdf72a53603a4ec5e14ecdc76e983c4466
MarkEdwardsPractice <markedwardspractice@gmail.com>
2024-05-10 11:04:59 +0900

    Make consistent format of code

--- script.js
+++ script.js
@@ -33,56 +33,52 @@ function playRound(humanChoice, computerChoice)
 {
     printChoices(humanChoice, computerChoice);
     if(humanChoice === computerChoice)
+    {
+        console.log("It's a tie!")
+    }
+    else if(humanChoice === "rock")
+    {
+        if(computerChoice === "paper")
         {
-            console.log("It's a tie!")
+            console.log("You lose!")
         }
-        else if(humanChoice === "rock")
-            {
-                if(computerChoice === "paper")
-                    {
-                        console.log("You lose!")
-                    }
-                    else
-                    {
-                        console.log("You win!");
-                    }
-            }
-            else if(humanChoice === "paper")
-                {
-                    if(computerChoice === "scissors")
-                        {
-                            console.log("You lose!");
-                        }
-                        else
-                        {
-                            console.log("You win!");
-                        }
-                }
-                else if(humanChoice === "scissors")
-                    {
-                        if(computerChoice === "paper")
-                            {
-                                console.log("You lose!");
-                            }
-                            else
-                            {
-                                console.log("You win!");
-                            }
-                    }
-                    else
-                    {
-                        console.log("Your selection not rock, paper, or scissors.");
-                    }
+        else
+        {
+            console.log("You win!");
+        }
+    }
+    else if(humanChoice === "paper")
+    {
+        if(computerChoice === "scissors")
+        {
+            console.log("You lose!");
+        }
+        else
+        {
+            console.log("You win!");
+        }
+    }
+    else if(humanChoice === "scissors")
+    {
+        if(computerChoice === "paper")
+        {
+            console.log("You lose!");
+        }
+        else
+        {
+            console.log("You win!");
+        }
+    }
+    else
+    {
+        console.log("Your selection not rock, paper, or scissors.");
+    }
 }
 function printChoices(humanChoice, computerChoice)
 {
     console.log("Your choice: " + humanChoice);
     console.log("Computer choice: " + computerChoice);
 }
-
-
-
-
 
 function playGame()
 {
